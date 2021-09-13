3238c52604cdd3ec43cdb2d1708e093afa781027
Dakota Sankey <dakotasankey@gmail.com>
2021-09-12 21:41:32 -0700

    fix

--- src/plugins/faq/$plugin.ts
+++ src/plugins/faq/$plugin.ts
@@ -1,5 +1,6 @@
 import { PluginGuilds } from 'porygon/plugin';
+import { getGuildNamesWithFaqEntries } from './impl/faq_list';
 import './impl/faq_list_data'; // ensure all created
 
-export default PluginGuilds.init('pokecom');
+export default PluginGuilds.init(...getGuildNamesWithFaqEntries());
 export { default as COMMAND_faq_faq } from './commands/faq';
