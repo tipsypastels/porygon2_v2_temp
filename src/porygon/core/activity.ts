bcdbbfb187ac9834299d61a3570975f8271af1cd
Dakota Sankey <dakotasankey@gmail.com>
2021-09-20 01:12:42 -0700

    stop doing activity every minute in dev

--- src/porygon/core/activity.ts
+++ src/porygon/core/activity.ts
@@ -1,11 +1,10 @@
 import { config } from 'porygon/config';
-import { DEV } from 'porygon/dev';
 import { Task, at } from 'porygon/schedule';
 import { random } from 'support/array';
 import { Porygon } from './client';
 
 const MESSAGES = config('activityMessages');
-const FREQ = at.every(DEV ? 1 : 30).minutes();
+const FREQ = at.every(30).minutes();
 
 const TASK = new Task({
   name: 'pory.activity',
