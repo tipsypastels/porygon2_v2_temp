694764951721ad1919784274bda5f8fd2101a791
Dakota Sankey <dakotasankey@gmail.com>
2021-10-21 01:22:32 -0700

    fix camel regex

--- src/plugins/duck/impl/camel.ts
+++ src/plugins/duck/impl/camel.ts
@@ -1,8 +1,7 @@
 import { clamp } from 'support/number';
 import { stripWhitespace } from 'support/string';
 
-const CAMEL = ['🐪', '🐫'];
-const PATTERN = new RegExp(`^[${CAMEL.join('|')}]+$`, 'm');
+const PATTERN = new RegExp('^[🐪🐫]+$', 'mu');
 const RESPONSE = 'yo '; // enshrined in ancient tradition
 const RESPONSE_MAX_COUNT = 100;
 const CHARS_PER_CAMEL = 2; // unicode emoji = 2 characters
